10eaec4cb87d5694a92f46609cfa700c1bf546ff
Jim Shaw <jimxshaw@gmail.com>
2015-08-07 22:29:17 -0500

    added routes

--- find-a-doc/routes/doctors.js
+++ find-a-doc/routes/doctors.js
@@ -5,4 +5,12 @@ router.get('/', function(req, res, next) {
   res.render('doctors');
 });
 
+router.get('/add', function(req, res, next) {
+  res.render('adddoctor');
+});
+
+router.get('/details/:id', function(req, res, next) {
+  res.render('details');
+});
+
 module.exports = router;

b81498289acfd13b764e6fee5fabf7f5a63bd159
Jim Shaw <jimxshaw@gmail.com>
2016-01-31 20:50:04 -0600

    installed express

--- node-web-api/index.js
+++ node-web-api/index.js
@@ -1,1 +1,44 @@
 var http = require('http');
+
+// The callback function that the createServer method contains is what's 
+// executed every time someone visits our web page. 
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Hello world!\n');
+}).listen(3000, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:3000/');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

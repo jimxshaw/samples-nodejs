22fa8aa7380baf571eb749186d06bc5ecf856b7b
Jim Shaw <jimxshaw@gmail.com>
2016-03-14 19:40:00 -0500

    added ability to POST a new file to the sample web server and to override the contents of existing files

--- misc/example1/server-compiled.js
+++ misc/example1/server-compiled.js
@@ -13,6 +13,13 @@ var invalidFileRegex = /^[.\/\\]|\.\./;
 // a request and returns a response. The server is a stream. To stop
 // it, we have to call .end method or it will continue to run indefinitely.
 var server = http.createServer(function (req, res) {
+    // Detects the method of form submission, either through the url (GET)
+    // or submit button (POST) and execute a specific method to handle each.
+    if (req.method === "POST") {
+        handlePost(req, res);
+        return;
+    }
+
     // Parse out the request's url query string.
     var query = url.parse(req.url, true).query;
 
@@ -85,6 +92,45 @@ function writeFile(req, res, query) {
         // If no error, write out with a status of 200, OK and get the contents
         // of the file out with buffer.
         writeText(res, 200, buffer.toString());
+    });
+}
+
+// POST requests are handled here.
+function handlePost(req, res) {
+    // Capture all of the request body data. The request object is a stream from the browser,
+    // it will stream in the data as it happens. We need to handle this asynchronously.
+    var body = "";
+
+    // Think of the req stream as the read stream while the res stream is the write stream.
+    // Because req is a read stream, we can add an .on method event handler. The event
+    // handler of "data" will be invoked every time the browser is sending more data to
+    // the server, aggregate the data as it flows in.
+    req.on("data", function (data) {
+        body += data;
+    });
+
+    // When the browser indicates to the server that there's no more data to send, we need
+    // to handle and process the data that we accumulated from all these data events.
+    req.on("end", function () {
+        var form = querystring.parse(body);
+
+        // If the user didn't provide a file name or the file name is tested to be invalid
+        // by our security regular expression, send back a status of 400 bad request.
+        if (!form.file || invalidFileRegex.test(form.file)) {
+            writeText(res, 400, "Invalid Path");
+            return;
+        }
+
+        // The writeFileSync is wrapped in a try-catch block. If anything goes wrong, the
+        // block will proc and an appropriate message is sent back. If it succeeds then
+        // kick off the writeIndex function that writes the file into the index page.
+        try {
+            fs.writeFileSync(path.join(directory, form.file), form.text);
+            writeIndex(req, res);
+        } catch (ex) {
+            writeText(res, 400, "Could not save file");
+            console.error(ex);
+        }
     });
 }
 
